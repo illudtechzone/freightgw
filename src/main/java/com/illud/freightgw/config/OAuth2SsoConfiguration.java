d87ea24fa0344ee4915482a3e346505d5c1f9609
Princekalathil <prince.k.c@lxisoft.com>
2019-09-19 12:50:09 +0530

    freight gateway 2nd commit

--- src/main/java/com/illud/freightgw/config/OAuth2SsoConfiguration.java
+++ src/main/java/com/illud/freightgw/config/OAuth2SsoConfiguration.java
@@ -2,6 +2,9 @@ package com.illud.freightgw.config;
 
 import com.illud.freightgw.security.AuthoritiesConstants;
 
+import io.github.jhipster.security.AjaxLogoutSuccessHandler;
+
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,6 +19,8 @@ import org.springframework.security.oauth2.client.OAuth2RestTemplate;
 import org.springframework.security.oauth2.client.resource.OAuth2ProtectedResourceDetails;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.security.web.csrf.CsrfFilter;
+import org.springframework.security.web.util.matcher.NegatedRequestMatcher;
+import org.springframework.security.web.util.matcher.RequestMatcher;
 import org.springframework.web.filter.CorsFilter;
 
 @EnableOAuth2Sso
@@ -24,13 +29,22 @@ import org.springframework.web.filter.CorsFilter;
 @Configuration
 public class OAuth2SsoConfiguration extends WebSecurityConfigurerAdapter {
 
-    private final CorsFilter corsFilter;
+	private final CorsFilter corsFilter;
+	private final RequestMatcher authorizationHeaderRequestMatcher;
 
-    public OAuth2SsoConfiguration(CorsFilter corsFilter) {
-        this.corsFilter = corsFilter;
-    }
+	public OAuth2SsoConfiguration(
+			@Qualifier("authorizationHeaderRequestMatcher") RequestMatcher authorizationHeaderRequestMatcher,
+			CorsFilter corsFilter) {
+		this.authorizationHeaderRequestMatcher = authorizationHeaderRequestMatcher;
+		this.corsFilter = corsFilter;
+	}
 
-    @Override
+	@Bean
+	public AjaxLogoutSuccessHandler ajaxLogoutSuccessHandler() {
+		return new AjaxLogoutSuccessHandler();
+	}
+
+	@Override
     public void configure(WebSecurity web) throws Exception {
         web.ignoring()
             .antMatchers(HttpMethod.OPTIONS, "/**")
@@ -38,34 +52,41 @@ public class OAuth2SsoConfiguration extends WebSecurityConfigurerAdapter {
             .antMatchers("/i18n/**")
             .antMatchers("/content/**")
             .antMatchers("/swagger-ui/index.html")
-            .antMatchers("/test/**");
+            .antMatchers("/test/**")
+        	.antMatchers("/h2-console/**");
     }
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-            .csrf()
-                .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
-            .and()
-            .addFilterBefore(corsFilter, CsrfFilter.class)
-            .headers()
-            .frameOptions()
-            .disable()
-        .and()
-            .authorizeRequests()
-            .antMatchers("/api/**").authenticated()
-            .antMatchers("/management/health").permitAll()
-            .antMatchers("/management/**").hasAuthority(AuthoritiesConstants.ADMIN)
-            .anyRequest().permitAll();
-    }
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+        .csrf()
+        .disable()
+        .addFilterBefore(corsFilter, CsrfFilter.class)
+        .headers()
+        .frameOptions()
+        .disable()
+    .and()
+        .logout()
+        .logoutUrl("/api/logout")
+        .logoutSuccessHandler(ajaxLogoutSuccessHandler())
+    .and()
+        .requestMatcher(new NegatedRequestMatcher(authorizationHeaderRequestMatcher))
+        .authorizeRequests()
+        .antMatchers("/api/auth-info").permitAll()
+        .antMatchers("/api/**").authenticated()
+        .antMatchers("/management/health").permitAll()
+        .antMatchers("/management/**").hasAuthority(AuthoritiesConstants.ADMIN)
+        .anyRequest().permitAll();
+	}
 
-    /**
-     * This OAuth2RestTemplate is used by org.springframework.cloud.security.oauth2.proxy.OAuth2TokenRelayFilter
-     * from Spring Cloud Security to refresh the access token when needed.
-     */
-    @Bean
-    public OAuth2RestTemplate oAuth2RestTemplate(OAuth2ProtectedResourceDetails oAuth2ProtectedResourceDetails,
-        OAuth2ClientContext oAuth2ClientContext) {
-        return new OAuth2RestTemplate(oAuth2ProtectedResourceDetails, oAuth2ClientContext);
-    }
+	/**
+	 * This OAuth2RestTemplate is used by
+	 * org.springframework.cloud.security.oauth2.proxy.OAuth2TokenRelayFilter from
+	 * Spring Cloud Security to refresh the access token when needed.
+	 */
+	@Bean
+	public OAuth2RestTemplate oAuth2RestTemplate(OAuth2ProtectedResourceDetails oAuth2ProtectedResourceDetails,
+			OAuth2ClientContext oAuth2ClientContext) {
+		return new OAuth2RestTemplate(oAuth2ProtectedResourceDetails, oAuth2ClientContext);
+	}
 }

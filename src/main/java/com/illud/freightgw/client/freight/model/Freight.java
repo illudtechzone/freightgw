cd217a6e85fc8568c48bc27b3c54ed214fc67f83
sahalr1 <35248765+sahalr1@users.noreply.github.com>
2019-09-28 10:17:00 +0530

    document annotation added in freight

--- src/main/java/com/illud/freightgw/client/freight/model/Freight.java
+++ src/main/java/com/illud/freightgw/client/freight/model/Freight.java
@@ -14,13 +14,16 @@ import org.springframework.validation.annotation.Validated;
 import javax.validation.Valid;
 import javax.validation.constraints.*;
 
+import org.springframework.data.elasticsearch.annotations.Document;
+
 /**
  * Freight
  */
 @Validated
 @javax.annotation.Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2019-09-27T15:11:08.290+05:30[Asia/Calcutta]")
-
-public class Freight   {
+@Document(indexName = "freight")
+    
+ public class Freight   {
   /**
    * Gets or Sets acceptedStatus
    */

dc44c940c0ef3275731b5114a4f2dcde4d698863
Princekalathil <prince.k.c@lxisoft.com>
2019-10-21 12:53:17 +0530

    all at document added in domain class

--- src/main/java/com/illud/freightgw/client/freight/model/Driver.java
+++ src/main/java/com/illud/freightgw/client/freight/model/Driver.java
@@ -9,6 +9,8 @@ import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import java.util.ArrayList;
 import java.util.List;
+
+import org.springframework.data.elasticsearch.annotations.Document;
 import org.springframework.validation.annotation.Validated;
 import javax.validation.Valid;
 import javax.validation.constraints.*;
@@ -18,7 +20,7 @@ import javax.validation.constraints.*;
  */
 @Validated
 @javax.annotation.Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2019-10-21T11:23:33.684+05:30[Asia/Calcutta]")
-
+@Document (indexName="driver")
 public class Driver   {
   @JsonProperty("company")
   private Company company = null;

1915726f7efa67ed4ad55181645405a91acf4ba5
Princekalathil <prince.k.c@lxisoft.com>
2019-10-26 20:43:45 +0530

    create @ document

--- src/main/java/com/illud/freightgw/client/freight/model/Customer.java
+++ src/main/java/com/illud/freightgw/client/freight/model/Customer.java
@@ -5,6 +5,8 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
+
+import org.springframework.data.elasticsearch.annotations.Document;
 import org.springframework.validation.annotation.Validated;
 import javax.validation.Valid;
 import javax.validation.constraints.*;
@@ -14,7 +16,7 @@ import javax.validation.constraints.*;
  */
 @Validated
 @javax.annotation.Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2019-10-26T20:11:06.451+05:30[Asia/Calcutta]")
-
+@Document (indexName="customer")
 public class Customer   {
   @JsonProperty("customerIdpCode")
   private String customerIdpCode = null;

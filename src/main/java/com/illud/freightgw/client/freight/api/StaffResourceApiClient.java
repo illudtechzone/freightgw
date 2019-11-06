b702c09afb840cf55a2fe0aa85b57858b96df7d5
Princekalathil <prince.k.c@lxisoft.com>
2019-11-06 10:11:35 +0530

    url change

--- src/main/java/com/illud/freightgw/client/freight/api/StaffResourceApiClient.java
+++ src/main/java/com/illud/freightgw/client/freight/api/StaffResourceApiClient.java
@@ -3,6 +3,6 @@ package com.illud.freightgw.client.freight.api;
 import org.springframework.cloud.openfeign.FeignClient;
 import com.illud.freightgw.client.freight.ClientConfiguration;
 
-@FeignClient(name="${freight.name:freight}", url="${freight.url:35.232.29.128:8085/}", configuration = ClientConfiguration.class)
+@FeignClient(name="${freight.name:freight}", url="${freight.url:35.193.1.70:8085/}", configuration = ClientConfiguration.class)
 public interface StaffResourceApiClient extends StaffResourceApi {
 }
